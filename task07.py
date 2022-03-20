4d382736f0a9a3d0bf4f57fd8d9c5055d171ea70
rupanshu05 <101396705+rupanshu05@users.noreply.github.com>
2022-03-20 22:53:47 +0530

    Add files via upload

---
+++ task07.py
@@ -0,0 +1,17 @@
+##task 7
+##explanation of methods 1)differences()
+##                       2)symmetic_diffeences()
+                       
+print("1.differences()")
+
+a = {1,2,3,4,5,6,7,8,9,10}
+b = {2,4,6,8,10}
+print(a,b)
+print(a.difference(b))
+
+
+print ("2 symmetric_difference()")
+c = {"a","b","c","d","f"}
+d = {"c","f","g","m","a"}
+print(c,d)
+print(c.symmetric_difference(d))
