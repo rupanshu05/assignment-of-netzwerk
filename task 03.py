2d8e9b4f2a7ee0be0600783010ded70af4ee22ba
rupanshu05 <101396705+rupanshu05@users.noreply.github.com>
2022-03-21 10:31:01 +0530

    Add files via upload

---
+++ task 03.py
@@ -0,0 +1,10 @@
+##task 03
+##add item 70 after 60 in the following python list
+##
+##input : var = [10,20,30,40,50,60,80,90,100]
+##output :[10,20,30,40,50,60,70,80,90,100]
+
+
+var = [10,20,30,40,50,60,80,90,100]
+var.insert(6,"70")
+print(var)
