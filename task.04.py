eae50b55995fb62d49a543732176426aa97b1f11
rupanshu05 <101396705+rupanshu05@users.noreply.github.com>
2022-03-21 10:19:24 +0530

    Add files via upload

---
+++ task.04.py
@@ -0,0 +1,10 @@
+##task 04
+##
+##add sublist[7,8] after 6
+##input = var = [1,2,3,4,5,6,9]
+##output =[1,2,3,4,5,6,7,8,9]
+
+var = ["1","2","3","4","5","6","9"]
+var.insert(6,"7")
+var.insert(7,"8")
+print(var)
